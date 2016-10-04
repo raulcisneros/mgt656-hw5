6be5a9406abdee351d056eebe034fc6122ef75b8
Raul Cisneros <raul.cisneros@yale.edu>
2016-10-04 23:38:30 +0000

    Add nickname route

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function(request, response){
+app.get('/', function(request, response) {
     response.send('Hello MGT-656!');
+});
+
+app.get('/nickname', function(request, response) {
+    response.send('wild-finch');
 });
 
 app.listen(process.env.PORT || 4000);
